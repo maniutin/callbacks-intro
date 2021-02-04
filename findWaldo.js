2d63df61d352a56c774d12cbeb74162aa6bf087b
Nick Maniutin <maniutin@gmail.com>
2021-02-04 18:28:40 +0000

    solves findWaldo

---
+++ findWaldo.js
@@ -0,0 +1,15 @@
+const findWaldo = function(names, found) {
+  names.forEach((element, index) => {
+    let name = element;
+    if (name === "Waldo") {
+      found(index);   // execute callback
+    }
+  }); {
+  }
+}
+
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}.`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
